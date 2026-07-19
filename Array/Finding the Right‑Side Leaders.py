a386c3da4c6a7b929d368fb6c8abe83bbf55f26f
Soubhik Sinha <91076835+S-rony@users.noreply.github.com>
2026-07-19 22:28:09 +0530

    Refactor leader finding algorithm with optimal solution
    Refactored the leader finding algorithm to include both brute force and optimal solutions. Added comments for clarity and improved code structure.

--- Array/Finding the Right‑Side Leaders.py
+++ Array/Finding the Right‑Side Leaders.py
@@ -1,13 +1,29 @@
 # Finding the Right_side leader
+#brute_force O(n²)
 arr = [16, 17, 4, 3, 5, 2]
 arr_1 = []
 arr_2 = []
-is_leader = True
+
 for i in range(len(arr) ):
+    is_leader = True
     for j in range(i+1, len(arr)):
         if arr[i] < arr[j]:
-            arr_1.append(arr[i])
-
-
+            is_leader = False
+            break
+    if is_leader:
+        arr_1.append(arr[i])
 
 print(arr_1)
+
+#optimal solution Time: O(n) Space: O(1)
+
+
+#transverse right to left
+
+max = arr[len(arr)-1]
+for k in range(len(arr) - 1, -1, -1):
+    if max <= arr[k]:
+        max = arr[k]
+        arr_2.append(max)
+arr_2.reverse()
+print(arr_2)
