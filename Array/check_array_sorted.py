f4861ddd48b57095bef6ef77d5aedfbcb6da297b
Soubhik Sinha <91076835+S-rony@users.noreply.github.com>
2026-07-21 11:21:31 +0530

    Fix is_sorted function logic for array sorting
    Refactor is_sorted function to correctly check if an array is sorted in ascending order.

--- Array/check_array_sorted.py
+++ Array/check_array_sorted.py
@@ -1,18 +1,19 @@
 def is_sorted(arr):
-    start = 0
-    end = len(arr) - 1
-    for i in range(len(arr)):
-        start+=1
-        if arr[start] >= arr[start+1]:
+    # start = 0
+    # end = len(arr) - 1
+    for i in range(len(arr) - 1):
 
-            return False
-        else:
-            return True
+        # start+=1
+        # sorted = True
+        if arr[i] > arr[i+1]:
+            sorted = False
+            break
+    else:
+        sorted = True
+    if sorted:
+        return True
+    else:
+        return False
 
-
-
-
-
-
-arr = [1,3,7,9]
+arr = [1,3,7,9,8]
 print(is_sorted(arr))
