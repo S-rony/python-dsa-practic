2158e8911dac9261086092142917b40eb89228ad
Soubhik Sinha <91076835+S-rony@users.noreply.github.com>
2026-07-28 22:19:49 +0530

    Implement optimal and brute force solutions for missing number
    Refactored the missing number solution to include an optimal approach using the expected sum formula and added a brute force solution.

--- Array/Missing Number Hunt.py
+++ Array/Missing Number Hunt.py
@@ -1,38 +1,59 @@
-class Solution:
-    def findMissingNumber(self, arr):
-        """
-        arr: List[int] - distinct integers from 1 to n with exactly one missing.
-        Returns the missing integer.
-        """
-        # Your implementation here
 
-        # max = float('-inf')
-        # min = float("+inf")
-        # for i in range(len(arr)):
-        #     if arr[i] > max:
-        #         max = arr[i]
-        #     if arr[i] < min:
-        #         min = arr[i]
-        # # for i in range(int(max), int(min), 1):
-        #     # new_ = max - i
-        #     print(i)
+arr = [5, 1, 3, 4]
+
+a = [9, 7, 6, 5, 4, 3, 2, 1, 8]
+
+#optimal Solution
+
+n = len(arr) + 1
+
+expected_sum_of_arr = (n*(n+1))//2
+sum_of_arr = ((n-1)*(n))//2
+Missing_number = expected_sum_of_arr - sum_of_arr
+print("OPTIMAL SOL",Missing_number)
+print()
 
 
 
-ar = [12, 11, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
-max = float('-inf')
-min = float("+inf")
-for i in range(len(ar)):
-    if ar[i] > max:
-        max = ar[i]
-    if ar[i] < min:
-        min = ar[i]
-fond = False
-for i in range(int(min), int(max)+1):
-    if i not in ar:
+
+
+
+
+
+n = len(arr) + 1
+for i in range(1,n + 1):
+    print(i)
+
+
+
+print()
+
+## Brute Force Solution
+n = len(a) + 1
+for i in range(1, n + 1):
+    if i not in a:
         print(i)
-        fond = True
-        break
-if not fond:
-    print(max+1)
+
+
+
+
+
+#My attempt
+# ar = [12, 11, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+# max = float('-inf')
+# min = float("+inf")
+# for i in range(len(ar)):
+#     if ar[i] > max:
+#         max = ar[i]
+#     if ar[i] < min:
+#         min = ar[i]
+# fond = False
+# for i in range(int(min), int(max)+1):
+#     if i not in ar:
+#         print(i)
+#         fond = True
+#         break
+# if not fond:
+#     print(max+1)
