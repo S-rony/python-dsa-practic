6f83e039a4ab38293762d608e0d4cdbb010b8878
Soubhik Sinha <91076835+S-rony@users.noreply.github.com>
2026-07-30 15:30:18 +0530

    Implement insertion and deletion in doubly linked list
    Added methods to insert at a specific position and delete nodes from the doubly linked list.

--- Doubly Linked List/revise.py
+++ Doubly Linked List/revise.py
@@ -30,8 +30,39 @@ class double_linked_list:
         self.head.prev = temp
         self.head = temp
 
-    def insert_at_med(self):
+    def insert_at_mid(self, value, x):
+        t = self.head
+        while t.next is not None:
+            if t.data == x:
+                break
+            else:
+                t = t.next
+        temp = Node(value)
+        temp.next = t.next
+        if t.next is not None:
+            t.next.prev = temp
+        t.next = temp
+        temp.prev = t
 
+    def deletion_doubly_ll(self, value):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        t = self.head
+        if t.data == value:
+            self.head = t.next
+            if self.head is not None:
+                self.head.prev = None
+            return
+        while t.next is not None:
+            if t.data == value:
+                t.prev.next = t.next
+                t.next.prev = t.prev
+                return
+            t = t.next
+        if t.data == value:
+            t.prev.next = None
+            return
 
 
 
@@ -43,6 +74,10 @@ class double_linked_list:
         print(t.data)
 
 
+
+
+
+
 obj = double_linked_list()
 
 obj.insert_at_beg(5)
@@ -51,6 +86,8 @@ obj.insert_at_end(20)
 obj.insert_at_end(30)
 obj.insert_at_end(40)
 obj.insert_at_end(50)
+obj.insert_at_mid(25,20)
+obj.deletion_doubly_ll(50)
 obj.print_dll()
 
 
